60ce7635c56eb8712f185ebd8a706121ba0fdde0
J R <rojames0402@gmail.com>
2021-01-04 19:06:17 +0000

    else and if statement

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #Write your code below this row 👇
 
 for fizz in range (1,101):
-  if fizz % 3 == 0:
-    print("Fizz")
-  if fizz % 5 == 0:
+  if fizz % 3 == 0 and fizz % 5:
+    print("FizzBuzz")
+  elif fizz % 5 == 0:
     print ("Buzz")
-  if fizz % 3 == 0 and fizz % 5 == 0:
-    print ("FizzBuzz")
-  print(fizz)
+  elif fizz % 3 == 0:
+    print ("Fizz")
+  else:
+    print(fizz)
