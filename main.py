8588945398fe3ea9e9352203b9e77a4f6bb5c314
J R <rojames0402@gmail.com>
2021-01-04 18:57:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Write your code below this row 👇
+
+for fizz in range (1,101):
+  if fizz % 3 == 0:
+    
+  if fizz % 5 == 0:
+    print ("Buzz")
+  if fizz % 3 == 0 & fizz % 5 == 0:
+    print ("FizzBuzz")
+
+print(fizz)
