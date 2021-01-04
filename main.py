6baa58f021068719d9d786a1fca6d56c5bd61f2a
J R <rojames0402@gmail.com>
2021-01-04 19:07:23 +0000

    condition of if statement

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Write your code below this row 👇
 
 for fizz in range (1,101):
-  if fizz % 3 == 0 and fizz % 5:
+  if fizz % 3 == 0 and fizz % 5 == 0:
     print("FizzBuzz")
   elif fizz % 5 == 0:
     print ("Buzz")
