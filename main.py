79456703db72989ee3e1abc6ba18f0fb2d6649c2
J R <rojames0402@gmail.com>
2021-01-04 19:04:49 +0000

    printing numbers

--- main.py
+++ main.py
@@ -2,10 +2,9 @@
 
 for fizz in range (1,101):
   if fizz % 3 == 0:
-    
+    print("Fizz")
   if fizz % 5 == 0:
     print ("Buzz")
-  if fizz % 3 == 0 & fizz % 5 == 0:
+  if fizz % 3 == 0 and fizz % 5 == 0:
     print ("FizzBuzz")
-
-print(fizz)
+  print(fizz)
